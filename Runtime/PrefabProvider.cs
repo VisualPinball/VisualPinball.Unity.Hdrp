f40d0595c66d693b015f14f10558b5fa68749821
freezy <freezy@kodi.tv>
2021-08-10 20:21:01 +0200

    prefabs: Add kicker prefabs.

--- Runtime/PrefabProvider.cs
+++ Runtime/PrefabProvider.cs
@@ -42,5 +42,53 @@ namespace VisualPinball.Engine.Unity.Hdrp
 					throw new ArgumentException(nameof(type), $"Unknown gate type {type}.");
 			}
 		}
+	public GameObject CreateTarget(int type)
+		{
+			switch (type)
+			{
+				case TargetType.DropTargetBeveled:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Drop Target - Beveled");
+				case TargetType.DropTargetFlatSimple:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Drop Target - Simple Flat");
+				case TargetType.DropTargetSimple:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Drop Target - Simple");
+				case TargetType.HitFatTargetRectangle:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Rectangle Fat");
+				case TargetType.HitFatTargetSlim:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Rectangle Fat Narrow");
+				case TargetType.HitFatTargetSquare:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Square Fat");
+				case TargetType.HitTargetRectangle:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Rectangle");
+				case TargetType.HitTargetRound:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Round");
+				case TargetType.HitTargetSlim:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Hit Target - Narrow");
+				default:
+					throw new ArgumentException(nameof(type), $"Unknown target type {type}.");
+			}
+		}
+
+		public GameObject CreateKicker(int type)
+		{
+			switch (type) {
+				case KickerType.KickerCup:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Cup");
+				case KickerType.KickerCup2:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Cup 2");
+				case KickerType.KickerGottlieb:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Gottlieb");
+				case KickerType.KickerHole:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Hole");
+				case KickerType.KickerHoleSimple:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Simple Hole");
+				case KickerType.KickerWilliams:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Williams");
+				case KickerType.KickerInvisible:
+					return UnityEngine.Resources.Load<GameObject>("Prefabs/Kicker - Invisible");
+				default:
+					throw new ArgumentException(nameof(type), $"Unknown kicker type {type}.");
+			}
+		}
 	}
 }
